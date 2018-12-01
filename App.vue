022ea7292efd35f0ca77994f7d5940b6971caf75
yehuo2003 <44639685+yehuo2003@users.noreply.github.com>
2018-12-02 01:11:17 +0800

    海澜之家移动端
    主配置文件上传

---
+++ App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="app-container">
+    <!-- App.vue 根组件 -->
+    <!-- 1.顶部加固定导航栏 -->
+    <mt-header fixed title="海澜之家"></mt-header>
+    <!-- 2.显示子组件 -->
+    <router-view></router-view>
+    <!-- 3.tabbar -->
+    <nav class="mui-bar mui-bar-tab">
+          <router-link class="mui-tab-item" to="/home">
+            <span class="mui-icon mui-icon-home"></span>
+            <span class="mui-tab-label">首页</span>
+          </router-link>
+          <router-link class="mui-tab-item" to="/home/cate">
+            <span class="mui-icon mui-icon-search"></span>
+            <span class="mui-tab-label">分类</span>
+          </router-link>
+           <router-link class="mui-tab-item" to="/cart">
+            <span class="mui-icon mui-icon-extra mui-icon-extra-cart"></span>
+            <span class="mui-tab-label">购物车</span>
+          </router-link>
+           <router-link class="mui-tab-item" to="/search">
+            <span class="mui-icon mui-icon-extra mui-icon-extra-cart"></span>
+            <span class="mui-tab-label">搜索</span>
+          </router-link>
+          <router-link class="mui-tab-item  mui-active" to="/user">
+            <span class="mui-icon mui-icon-contact">
+          </span>
+            <span class="mui-tab-label">我的</span>
+          </router-link>
+        </nav>    
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+<style>
+  .app-container{
+    padding-top: 40px;
+    padding-bottom: 50px;
+    overflow-x: hidden;
+  }
+</style>
---
+++ main.js
@@ -0,0 +1,41 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+//引入mint-ui组件并注册
+//1.引入Header组件
+import { Header,Swipe,SwipeItem,Button} from 'mint-ui';
+//引入mint-ui 样式文件
+import 'mint-ui/lib/style.css';
+//导入 MUI 样式表
+//还需要加载字体图标
+import './lib/mui/css/icons-extra.css'
+Vue.config.productionTip = false
+//2.注册Header组件
+Vue.component(Header.name,Header);
+Vue.component(Swipe.name,Swipe);
+Vue.component(SwipeItem.name,SwipeItem,Button);
+Vue.component(Button.name,Button);
+import './lib/mui/css/mui.css'
+
+Vue.config.productionTip = false
+//3.导入 vue-resource 引入到项目中
+import VueResource from 'vue-resource'
+//4.加载VueResource  全部加载
+Vue.use(VueResource)
+//5.自定义过滤器
+//-dateFormat 过滤器名称
+//-datestr 用户日期
+Vue.filter("dateFormat",function(datestr,pattern="YYYY-MM-DD"){
+  return new Date(datestr).toLocaleString();
+});
+//6.设置全局ajax访问基础路径
+Vue.http.options.root = "http://127.0.0.1:3000";
+
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
